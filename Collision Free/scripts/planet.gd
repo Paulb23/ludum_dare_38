ce97864ee45b7045b7b45c002daa9db472ca3343
Paulb23 <p_batty@hotmail.co.uk>
2017-04-23 15:30:14 +0100

    Flashing on dmg

--- Collision Free/scripts/planet.gd
+++ Collision Free/scripts/planet.gd
@@ -1,6 +1,11 @@
 extends Node2D
 
 var health = 50
+var clear_color
+
+func _ready():
+	clear_color = VisualServer.get_default_clear_color();
+	get_node("flash_timer").connect("timeout", self, "flash_timer")
 
 func _fixed_process(delta):
 	if not get_node("AnimationPlayer").is_playing():
@@ -11,6 +16,9 @@ func hit(dmg):
 	health -= dmg
 	var percent = (float(((float(health)/float(50))*float(100))/float(100)))
 	get_node("health").set_percentage(percent)
+	VisualServer.set_default_clear_color(Color("88495a"));
+	get_node("flash_timer").start();
+
 	if health <= 0:
 		set_pause_mode(PAUSE_MODE_PROCESS)
 		get_node("AnimationPlayer").set_pause_mode(PAUSE_MODE_PROCESS)
@@ -21,3 +29,6 @@ func hit(dmg):
 		Globals.set("CURRENT_WAVE", get_parent().current_wave)
 		set_fixed_process(true)
 		pass
+
+func flash_timer():
+	VisualServer.set_default_clear_color(clear_color);
